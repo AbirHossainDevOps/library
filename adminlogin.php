71471d70a3fce6d842744aeb7c238af5a36406b3
Abir Hossain <131145953+AbirHossainDevOps@users.noreply.github.com>
2023-05-24 09:24:25 +0600

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -81,7 +81,7 @@ echo "<script>alert('Invalid Details');</script>";
 </div>
  <div class="form-group">
 <label>Verification code : </label>
-<input type="text"  name="vercode" maxlength="5" autocomplete="off" required style="width: 150px; height: 25px;" />&nbsp;<img src="captcha.php">
+<input type="text"  name="vercode" maxlength="5" autocomplete="off" required style="width: 150px; height: 25px;" />&nbsp;<?php echo "<h2>99999</h2>";?>
 </div>  
 
  <button type="submit" name="login" class="btn btn-info">LOGIN </button>
