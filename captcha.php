15848a9081bfa49d762015c81dd38bf21e04634d
Abir Hossain <131145953+AbirHossainDevOps@users.noreply.github.com>
2023-05-24 09:19:36 +0600

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -3,10 +3,5 @@ header("Content-Type: image/png");
 session_start(); 
 $text = 99999; 
 $_SESSION["vercode"] = $text; 
-$im = @imagecreate(110, 20)
-    or die("Cannot Initialize new GD image stream");
-$background_color = imagecolorallocate($im, 0, 0, 0);
-$text_color = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  $text, $text_color);
-imagepng($im);
+echo "<h2>99999</h2>";
 ?>
