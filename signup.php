ccaeaf21e47ec9fa6b8063c6036d17959dd72f29
Abir Hossain <131145953+AbirHossainDevOps@users.noreply.github.com>
2023-05-25 10:17:11 +0600

    Update signup.php

--- signup.php
+++ signup.php
@@ -142,7 +142,7 @@ error:function (){}
 </div>
  <div class="form-group">
 <label>Verification code : </label>
-<input type="text"  name="vercode" maxlength="5" autocomplete="off" required style="width: 150px; height: 25px;" />&nbsp;<?php echo "<h2>The code is 99999</h2>";?>
+<input type="text"  name="vercode" maxlength="5" autocomplete="off" required style="width: 150px; height: 25px;" />&nbsp;<img src="captcha.php">
 </div>                                
 <button type="submit" name="signup" class="btn btn-danger" id="submit">Register Now </button>
 
