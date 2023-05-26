4905b55b1c8bae81ad7b7b31b5f4fa541e3cd856
Abir Hossain <131145953+AbirHossainDevOps@users.noreply.github.com>
2023-05-26 16:26:43 +0600

    Update index.php
    Made some changes to trigger

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ echo "<script>alert('Invalid Details');</script>";
 </div>
 
  <div class="form-group">
-<label>Verification code : </label>
+<label>Add Verification code : </label>
 <input type="text" class="form-control1"  name="vercode" maxlength="5" autocomplete="off" required  style="height:25px;" />&nbsp;<img src="captcha.php">&nbsp; <?php echo $_SESSION["vercode"]?>
  </div> 
 
